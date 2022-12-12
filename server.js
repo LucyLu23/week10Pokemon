e728ebfb87639f5626007747dea9733485ca5d9f
LucyLu23 <114943897+LucyLu23@users.noreply.github.com>
2022-12-12 16:54:02 -0700

    morecompletion

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const pokemon =  require('./models/pokemon');
 const port =3001;
 
 app.get('/',(req, res) => res.send('Welcome to the Pokemon App!'));
-app.get('/pokemon',(req, res) => res.send('pokemon'));
+app.get('/models/pokemon',(req, res) => res.send(pokemon));
+app.get('/views/Index',(req, res) => res.send(Index));
 
 app.set('view engine','jsx');
 app.engine('jsx', require('express-react-views').createEngine());
-
 
 
 
